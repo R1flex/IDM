1d78585807464f527406057f6f4c1d307912bc71
R1flex <79039756+R1flex@users.noreply.github.com>
2021-02-14 12:50:00 +0400

    Add files via upload

---
+++ lptoha-master/objects/ignored_members.py
@@ -0,0 +1,39 @@
+from typing import Dict
+from objects.base import BaseModel
+
+__all__ = (
+    'IgnoredMembers',
+    'IgnoredGlobalMembers',
+    'MutedMembers',
+)
+
+
+class IgnoredMembers(BaseModel):
+    member_id: int
+    chat_id: int
+
+    def save(self) -> Dict[str, int]:
+        return {
+            'member_id': self.member_id,
+            'chat_id': self.chat_id,
+        }
+
+
+class IgnoredGlobalMembers(BaseModel):
+    member_id: int
+
+    def save(self) -> Dict[str, int]:
+        return {
+            'member_id': self.member_id,
+        }
+
+
+class MutedMembers(BaseModel):
+    member_id: int
+    chat_id: int
+
+    def save(self) -> Dict[str, int]:
+        return {
+            'member_id': self.member_id,
+            'chat_id': self.chat_id,
+        }
